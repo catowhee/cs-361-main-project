c83db8f20a971689d2a4bc8ed75a4a168372b684
catowhee <81670803+catowhee@users.noreply.github.com>
2024-11-02 20:05:32 -0700

    Added jQuery & Select2

--- static/js/script.js
+++ static/js/script.js
@@ -1,52 +1,58 @@
-console.log("hello");
+$(document).ready(function(){
 
-// Initialize Flatpickr for date picker (date only)
-flatpickr("#start-date", {
-    enableTime: false,        // Date only, no time
-    dateFormat: "l, F n",       // Format as YYYY-MM-DD
-    defaultDate: "Thursday, November 11"
-});
+    console.log("hello");
 
-// Initialize Flatpickr for time picker (time only)
-flatpickr("#start-time", {
-    enableTime: true,         // Time only
-    noCalendar: true,         // No calendar, just time picker
-    dateFormat: "H:i",        // Format as HH:MM
-    time_24hr: true           // Use 24-hour format
-});
+    //$('.js-example-basic-multiple').select2();
+  
 
-function add_opening_to_data(){
-    const start_date =  document.getElementById('start-date').value;
-    const start_time =  document.getElementById('start-time').value;
-    const duration =  document.getElementById('duration').value;
-    const opening = {
-        "start_date": start_date,
-        "start_time": start_time,
-        "duration": duration,
-        "timezone": user_tz
+    // Initialize Flatpickr for date picker (date only)
+    flatpickr("#start-date", {
+        enableTime: false,        // Date only, no time
+        dateFormat: "l, F n",       // Format as YYYY-MM-DD
+        defaultDate: "Thursday, November 11"
+    });
+    
+    // Initialize Flatpickr for time picker (time only)
+    flatpickr("#start-time", {
+        enableTime: true,         // Time only
+        noCalendar: true,         // No calendar, just time picker
+        dateFormat: "H:i",        // Format as HH:MM
+        time_24hr: true           // Use 24-hour format
+    });
+      
+    function add_opening_to_data(){
+        const start_date =  document.getElementById('start-date').value;
+        const start_time =  document.getElementById('start-time').value;
+        const duration =  document.getElementById('duration').value;
+        const opening = {
+            "start_date": start_date,
+            "start_time": start_time,
+            "duration": duration,
+            "timezone": user_tz
+        }
+        openings.push(opening)
+        console.log(openings);
     }
-    openings.push(opening)
-    console.log(openings);
-}
-
-function add_opening_to_table(){
-    const opening_table =  document.getElementById('openings-table');
-    // If table doesn't have data yet, we'll have additional steps
-    const fresh_table = opening_table.querySelector('thead tr') == null;
-    console.log("Fresh table");
-
-}
-
-const user_tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
-const openings = []
-const form = document.getElementById("add-opening-form");
-
-// Add opening to openings array when form is submitted
-form.addEventListener('submit', function(event){
-    event.preventDefault();
-    add_opening_to_data();
-    add_opening_to_table();
+    
+    function add_opening_to_table(){
+        const opening_table =  document.getElementById('openings-table');
+        // If table doesn't have data yet, we'll have additional steps
+        const empty_table = opening_table.querySelector('thead tr') == null;
+        if (empty_table) {
+    
+        }
+        console.log("Fresh table");
+    
+    }
+    
+    const user_tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const openings = []
+    const form = document.getElementById("add-opening-form");
+    
+    // Add opening to openings array when form is submitted
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        add_opening_to_data();
+        add_opening_to_table();
+    });
 });
-
-
-
